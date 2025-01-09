38dfce15c370baf4e43c58bd95953b39e3ce04b2
Mario Wegmann <mario.wegmann@tha.de>
2025-01-10 00:37:09 +0100

    Added Buttons for Grid Layout Control

--- components/custom-ui/liveControl.tsx
+++ components/custom-ui/liveControl.tsx
@@ -10,35 +10,68 @@ interface LiveControlProps {
 
 export default function LiveControl({isConnected, setShouldWSReconnect, wantLiveUpdate, setWantLiveUpdate, setShouldClearMessages}: LiveControlProps) {
     return (
-        <div className="p-2" style={{display: "flex", justifyContent: "space-between"}}>
-            <button
-                type="button"
-                className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 disabled:bg-gray-500"
-                style={{display: "flex", justifyContent: "center"}}
-                onClick={() => setShouldWSReconnect(true)}
-                disabled={isConnected}
-                    >
-                <RefreshCcw/>
-            </button>
-            <div className="p-4"></div>
-            <button
-                type="button"
-                className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                style={{display: "flex", justifyContent: "center"}}
-                onClick={() => setShouldClearMessages(true)}
-            >
-                <Trash2/>
-            </button>
-            <div className="p-4"></div>
+        <div>
+    <div className="p-2" style={{display: "flex", justifyContent: "space-between"}}>
+        <button
+            type="button"
+            className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 disabled:bg-gray-500"
+            style={{display: "flex", justifyContent: "center"}}
+            onClick={() => setShouldWSReconnect(true)}
+            disabled={isConnected}
+        >
+            <RefreshCcw/>
+        </button>
+        <div className="p-4"></div>
+        <button
+            type="button"
+            className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            style={{display: "flex", justifyContent: "center"}}
+            onClick={() => setShouldClearMessages(true)}
+        >
+            <Trash2/>
+        </button>
+        <div className="p-4"></div>
 
-            <button
-                type="button"
-                className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                style={{display: "flex", justifyContent: "center"}}
-                onClick={() => setWantLiveUpdate(!wantLiveUpdate)}
-            >
-                {wantLiveUpdate ? <Pause />  : <Play/>}
-            </button>
+        <button
+            type="button"
+            className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            style={{display: "flex", justifyContent: "center"}}
+            onClick={() => setWantLiveUpdate(!wantLiveUpdate)}
+        >
+            {wantLiveUpdate ? <Pause/> : <Play/>}
+        </button>
+    </div>
+    <div className="p-2" style={{display: "flex", justifyContent: "space-between"}}>
+        <button
+            type="button"
+            className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 disabled:bg-gray-500"
+            style={{display: "flex", justifyContent: "center"}}
+            onClick={() => setShouldWSReconnect(true)}
+            disabled={isConnected}
+        >
+            <RefreshCcw/>
+        </button>
+        <div className="p-4"></div>
+        <button
+            type="button"
+            className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            style={{display: "flex", justifyContent: "center"}}
+            onClick={() => setShouldClearMessages(true)}
+        >
+            <Trash2/>
+        </button>
+        <div className="p-4"></div>
+
+        <button
+            type="button"
+            className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            style={{display: "flex", justifyContent: "center"}}
+            onClick={() => setWantLiveUpdate(!wantLiveUpdate)}
+        >
+            {wantLiveUpdate ? <Pause/> : <Play/>}
+        </button>
+    </div>
         </div>
-    );
+)
+    ;
 };
