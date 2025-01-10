95b2b432f706841d423beab9f1644c4ae1c4bb9d
Mario Wegmann <mario.wegmann@tha.de>
2025-01-10 15:35:23 +0100

    Fixed Sidebar DBCControl Layout

--- components/LiveView/DBCControl.tsx
+++ components/LiveView/DBCControl.tsx
@@ -1,8 +1,6 @@
-import {DBCData} from "@/components/DBCParser/DBCParser";
-
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { DBCData } from "@/components/DBCParser/DBCParser";
+import { ScrollArea } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
-
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -20,33 +18,44 @@ interface DBCControlProps {
 
 export default function DBCControl({ dbcData, onAddWidget }: DBCControlProps) {
     return (
-        <div className="">
+        <div className="h-full flex flex-col">
             <h3 className="text-lg font-semibold mb-2">DBC Control</h3>
             {dbcData ? (
-                <div className="space-y-4">
-                    <div className="text-sm text-gray-500">Version: {dbcData.version}</div>
-                    <ScrollArea className=" w-[250px] h-[300px] rounded-md border p-1">
-                        <div className="mt-2">
-                            <span onClick={() => onAddWidget("Table", -1, -1)} className="font-medium hover:underline hover:cursor-pointer">All CAN messages</span>
-                            <Separator className="my-3"/>
-                        </div>
+                <div className="flex flex-col min-h-0">
+                    <div className="text-sm text-gray-500 mb-4">Version: {dbcData.version}</div>
+                    <ScrollArea className="flex-1">
+                        <div className="pr-4">
+                            <div className="mt-2">
+                                <span
+                                    onClick={() => onAddWidget("Table", -1, -1)}
+                                    className="font-medium hover:underline hover:cursor-pointer"
+                                >
+                                    All CAN messages
+                                </span>
+                                <Separator className="my-3"/>
+                            </div>
                             {dbcData.messages.map((message, index) => (
                                 <div key={index}>
-                                    <span onClick={() => onAddWidget("Signals", message.id, -1)}
-                                          className="font-medium hover:underline hover:cursor-pointer">{message.name}</span>
+                                    <span
+                                        onClick={() => onAddWidget("Signals", message.id, -1)}
+                                        className="font-medium hover:underline hover:cursor-pointer"
+                                    >
+                                        {message.name}
+                                    </span>
                                     <span className="pl-2 text-sm text-gray-500">
-                                            ID: {message.id}
-                                        </span>
+                                        ID: {message.id}
+                                    </span>
                                     {message.signals.map((signal, sigIndex) => (
                                         <DropdownMenu key={sigIndex}>
                                             <DropdownMenuTrigger asChild>
                                                 <div className="pl-2 rounded-md">
                                                     <Separator className="my-1"/>
-                                                    <span
-                                                        className="font-medium hover:underline hover:cursor-pointer">{signal.name}</span>
+                                                    <span className="font-medium hover:underline hover:cursor-pointer">
+                                                        {signal.name}
+                                                    </span>
                                                     <span className="pl-2 pr-8 text-sm text-gray-500">
-                                                    Start: {signal.startBit}
-                                                </span>
+                                                        Start: {signal.startBit}
+                                                    </span>
                                                 </div>
                                             </DropdownMenuTrigger>
                                             <DropdownMenuContent className="w-56">
@@ -54,23 +63,28 @@ export default function DBCControl({ dbcData, onAddWidget }: DBCControlProps) {
                                                 <DropdownMenuSeparator/>
                                                 <DropdownMenuGroup>
                                                     <DropdownMenuItem
-                                                        onClick={() => onAddWidget("Number", message.id, sigIndex)}>
+                                                        onClick={() => onAddWidget("Number", message.id, sigIndex)}
+                                                    >
                                                         Number
                                                     </DropdownMenuItem>
                                                     <DropdownMenuItem
-                                                        onClick={() => onAddWidget("Binary", message.id, sigIndex)}>
+                                                        onClick={() => onAddWidget("Binary", message.id, sigIndex)}
+                                                    >
                                                         Binary
                                                     </DropdownMenuItem>
                                                     <DropdownMenuItem
-                                                        onClick={() => onAddWidget("Hex", message.id, sigIndex)}>
+                                                        onClick={() => onAddWidget("Hex", message.id, sigIndex)}
+                                                    >
                                                         Hex
                                                     </DropdownMenuItem>
                                                     <DropdownMenuItem
-                                                        onClick={() => onAddWidget("Gauge", message.id, sigIndex)}>
+                                                        onClick={() => onAddWidget("Gauge", message.id, sigIndex)}
+                                                    >
                                                         Gauge
                                                     </DropdownMenuItem>
                                                     <DropdownMenuItem
-                                                        onClick={() => onAddWidget("LineChart", message.id, sigIndex)}>
+                                                        onClick={() => onAddWidget("LineChart", message.id, sigIndex)}
+                                                    >
                                                         LineChart
                                                     </DropdownMenuItem>
                                                 </DropdownMenuGroup>
@@ -80,11 +94,12 @@ export default function DBCControl({ dbcData, onAddWidget }: DBCControlProps) {
                                     <Separator className="my-3"/>
                                 </div>
                             ))}
-                            <ScrollBar orientation="horizontal"/>
+                        </div>
                     </ScrollArea>
-                </div>) : (
+                </div>
+            ) : (
                 <div>Lade DBC Daten...</div>
-                )}
+            )}
         </div>
     )
 }
--- components/LiveView/Sidebar.tsx
+++ components/LiveView/Sidebar.tsx
@@ -1,11 +1,7 @@
-
 import DBCControl from './DBCControl'
 import GridControl from './GridControl'
-
 import { Card } from '@/components/ui/card'
-
 import { DBCData } from "@/components/DBCParser/DBCParser";
-
 
 interface SidebarProps {
     isWSConnected: boolean;
@@ -17,23 +13,33 @@ interface SidebarProps {
     dbcData: DBCData | null;
 }
 
-export default function ownSidebar({isWSConnected, setShouldWSReconnect, wantLiveUpdate, setWantLiveUpdate, setShouldClearMessages, onAddWidget, dbcData}: SidebarProps) {
+export default function ownSidebar({
+                                       isWSConnected,
+                                       setShouldWSReconnect,
+                                       wantLiveUpdate,
+                                       setWantLiveUpdate,
+                                       setShouldClearMessages,
+                                       onAddWidget,
+                                       dbcData
+                                   }: SidebarProps) {
     return (
-        <div className="w-100 border-r bg-gray-50 p-4 flex justify-end flex-col">
-            <Card className="mb-4 p-4">
-                <GridControl isWSConnected={isWSConnected}
-                             setShouldWSReconnect={setShouldWSReconnect}
-                             wantLiveUpdate={wantLiveUpdate}
-                             setWantLiveUpdate={setWantLiveUpdate}
-                             setShouldClearMessages={setShouldClearMessages}
+        <div className="w-[310px] border-r bg-gray-50 p-4 flex flex-col">
+            <Card className="shrink-0 mb-4 p-4">
+                <GridControl
+                    isWSConnected={isWSConnected}
+                    setShouldWSReconnect={setShouldWSReconnect}
+                    wantLiveUpdate={wantLiveUpdate}
+                    setWantLiveUpdate={setWantLiveUpdate}
+                    setShouldClearMessages={setShouldClearMessages}
                 />
             </Card>
-            <Card className="mb-4 p-4 max-h-full flex-grow-[4]">
-                <DBCControl onAddWidget={onAddWidget}
-                            dbcData={dbcData}
+            <Card className="flex-1 min-h-0 mb-4 p-4">
+                <DBCControl
+                    onAddWidget={onAddWidget}
+                    dbcData={dbcData}
                 />
             </Card>
-            <div className="text-center">
+            <div className="shrink-0 text-center text-sm text-gray-500">
                 created by TI 5 in WiSe 2024/25
             </div>
         </div>
