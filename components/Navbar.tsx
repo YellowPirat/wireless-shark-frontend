019fba41a45dab0933ce11d27f195920be056069
Mario Wegmann <mario.wegmann@tha.de>
2025-01-13 03:06:23 +0100

    Switched to can instead of vcan

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,37 +13,37 @@ export default function Navbar() {
     return (
         <div className="w-full max-w-full flex justify-between px-4 py-2 bg-gray-50 border-b">
             <NavigationMenu className="">
-                <Link href="/live-view/vcan0" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
+                <Link href="/live-view/can0" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         <LineChart className="w-4 h-4 mr-2"/>
                         Live 0
                     </NavigationMenuLink>
                 </Link>
-                <Link href="/live-view/vcan1" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
+                <Link href="/live-view/can1" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         <LineChart className="w-4 h-4 mr-2"/>
                         Live 1
                     </NavigationMenuLink>
                 </Link>
-                <Link href="/live-view/vcan2" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
+                <Link href="/live-view/can2" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         <LineChart className="w-4 h-4 mr-2"/>
                         Live 2
                     </NavigationMenuLink>
                 </Link>
-                <Link href="/live-view/vcan3" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
+                <Link href="/live-view/can3" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         <LineChart className="w-4 h-4 mr-2"/>
                         Live 3
                     </NavigationMenuLink>
                 </Link>
-                <Link href="/live-view/vcan4" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
+                <Link href="/live-view/can4" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         <LineChart className="w-4 h-4 mr-2"/>
                         Live 4
                     </NavigationMenuLink>
                 </Link>
-                <Link href="/live-view/vcan5" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
+                <Link href="/live-view/can5" legacyBehavior passHref className="px-4 py-2 hover:bg-gray-100 rounded">
                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         <LineChart className="w-4 h-4 mr-2"/>
                         Live 5
